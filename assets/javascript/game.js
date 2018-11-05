b15d2b5704e746df5e9c0e6e959b6059cb837c2d
IamMonkie <43730745+IamMonkie@users.noreply.github.com>
2018-11-05 18:54:41 -0500

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -115,25 +115,3 @@ $(document).ready(function () {
 
     
 }); // ready close
-
-
-
-
-
-
-const words = ['John', 'Jacob', 'Jingleheimersmith']
-
-function chooseWord(array) {
-    return array[Math.floor(Math.random() * array.length)].toUpperCase()
-}
-
-function fillInWordWithLetter(letter, word) {
-    return word.toLowerCase().split('').map(l => {
-        if (l === letter) {
-            return l.toUpperCase()
-        }
-        return '_'
-    }).join(' ')
-}
-
-fillInWordWithLetter('j', chooseWord(words))
